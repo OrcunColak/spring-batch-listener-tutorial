b59fcaba84ba3b1e24b8c2652c5b5eb67c45653d
Orçun Çolak <orcun.colak@hazelcast.com>
2024-10-01 09:26:32 +0300

    add comments

--- src/main/java/com/colak/springtutorial/config/steplistener/CustomStepExecutionListener.java
+++ src/main/java/com/colak/springtutorial/config/steplistener/CustomStepExecutionListener.java
@@ -1,21 +1,25 @@
 package com.colak.springtutorial.config.steplistener;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.core.StepExecutionListener;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class CustomStepExecutionListener implements StepExecutionListener {
 
     @Override
     public void beforeStep(StepExecution stepExecution) {
-        System.out.println("Starting step: " + stepExecution.getStepName());
+        log.info("Starting step: {}", stepExecution.getStepName());
     }
 
+    // afterStep method returns ExitStatus. This exitStatus will be populated in BATCH_STEP_EXECUTION table
+    // after step is executed.
     @Override
     public ExitStatus afterStep(StepExecution stepExecution) {
-        System.out.println("Finished step: " + stepExecution.getStepName());
+        log.info("Finished step: {}", stepExecution.getStepName());
         return stepExecution.getExitStatus();
     }
 }
